a4ce8f55ca84be23bbb9a4d6e776c97184bb42a3
Maxi Colqui <108244836+maxicolqui@users.noreply.github.com>
2023-07-05 00:48:17 -0300

    Add files via upload

---
+++ js/palabras.js
@@ -0,0 +1,3 @@
+const words = ['Qatar','Ecuador', 'Senegal','Países Bajos','Inglaterra','Iran','Estados Unidos','Gales','Argentina','Arabia Saudita','Mexico','Polonia','Francia','Australia',
+'Dinamarca','Tunez','España','Costa Rica','Alemania','Japon','Belgica','Canada','Marruecos','Croacia','Brasil','Serbia','Suiza','Camerun',
+'Portugal','Ghana','Uruguay','Corea del Sur'];
